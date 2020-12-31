b9f43deaffd5720c3bd8819350cf8760360f9f74
Shane <>
2020-12-31 13:40:44 -0500

    changed character type for notes. Changed length for models

--- main/models.py
+++ main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=24)
     completed = models.BooleanField(default=False)
-    notes = models.TextField()
+    notes = models.CharField(max_length=80)
     created = models.DateTimeField( auto_now=False, auto_now_add=True)
     
     def __str__(self):
